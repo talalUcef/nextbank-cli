cfbd603dc6bd5a63f2e4f8279254c193feae934b
tallalUcef <youssef.tallal@viseo.com>
2018-11-11 22:26:09 +0100

    [US-1] Deposit service

---
+++ src/main/java/com/nextbank/cli/command/AccountCommands.java
@@ -0,0 +1,44 @@
+package com.nextbank.cli.command;
+
+import com.nextbank.cli.helper.ConsoleHelper;
+import com.nextbank.cli.helper.Messages;
+import com.nextbank.cli.service.AccountService;
+import com.nextbank.cli.service.AuthenticationService;
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+import java.math.BigDecimal;
+
+@ShellComponent
+class AccountCommands {
+
+    private final ConsoleHelper console;
+    private final AuthenticationService authenticationService;
+    private final AccountService accountService;
+    private final Messages messages;
+
+    AccountCommands(ConsoleHelper console, AuthenticationService authenticationService,
+                    AccountService accountService, Messages messages) {
+        this.console = console;
+        this.authenticationService = authenticationService;
+        this.accountService = accountService;
+        this.messages = messages;
+    }
+
+    @ShellMethod("Deposit money into your account")
+    public void deposit(BigDecimal amount) {
+        try {
+            this.accountService.depositIntoCurrentAccount(amount);
+            console.write(messages.get("deposit.success"));
+        } catch (IllegalArgumentException | IllegalStateException e) {
+            this.console.write(e.getMessage());
+        }
+    }
+
+    Availability depositAvailability() {
+        return this.authenticationService.isConnected() ?
+                Availability.available() :
+                Availability.unavailable(messages.get("customer.not.connected.message"));
+    }
+}
---
+++ src/main/java/com/nextbank/cli/service/AccountService.java
@@ -0,0 +1,77 @@
+package com.nextbank.cli.service;
+
+import com.nextbank.cli.domain.Account;
+import com.nextbank.cli.domain.AccountOperation;
+import com.nextbank.cli.domain.Customer;
+import com.nextbank.cli.enums.AccountOperationType;
+import com.nextbank.cli.helper.Messages;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Date;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class AccountService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AccountService.class);
+
+    private final AuthenticationService authenticationService;
+    private final Messages messages;
+    private final AtomicLong ids = new AtomicLong();
+
+    public AccountService(AuthenticationService authenticationService, Messages messages) {
+        this.authenticationService = authenticationService;
+        this.messages = messages;
+    }
+
+    public void depositIntoCurrentAccount(BigDecimal amount) {
+
+        this.validateOperationAmount(amount);
+
+        final Account account = this.getCurrentAccount();
+
+        final BigDecimal newBalance = account.getBalance()
+                .add(amount).setScale(2, RoundingMode.HALF_EVEN);
+
+        final var credit = new AccountOperation(ids.incrementAndGet(),
+                new Date(), amount, newBalance, AccountOperationType.CREDIT);
+        LOG.info("Account {} has a new credit operation {}", account.getAccountNumber(), credit);
+        account.getJournal().add(credit);
+
+        account.setBalance(newBalance);
+
+        LOG.info("Deposit of {} into account {}, its new balance is {}",
+                amount, account.getAccountNumber(), account.getBalance());
+    }
+
+    private Account getCurrentAccount() {
+
+        Account connectedCustomerAccount = null;
+
+        if (authenticationService.isConnected()) {
+            final Optional<Customer> connectedCustomer = authenticationService.getConnectedCustomer();
+            if (connectedCustomer.isPresent()) {
+                connectedCustomerAccount = connectedCustomer.get().getAccount();
+            }
+        }
+
+        if (connectedCustomerAccount == null) {
+            LOG.error("Account unavailable");
+            throw new IllegalStateException(this.messages.get("account.unavailable"));
+        }
+
+        return connectedCustomerAccount;
+    }
+
+    private void validateOperationAmount(BigDecimal amount) {
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            LOG.error("Trying to make operation with an invalid amount of {}", amount);
+            throw new IllegalArgumentException(this.messages.get("amount.invalid"));
+        }
+    }
+}
